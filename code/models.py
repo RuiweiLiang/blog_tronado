974162bd8b2ac7b2b66e9d1a6a6a58f6b142b3aa
945349211@qq.com <945349211@qq.com>
2021-02-04 10:44:18 +0800

    修改前端逻辑

--- code/models.py
+++ code/models.py
@@ -5,7 +5,9 @@ from settings import settings
 # cursor = db.cursor()
 # cursor.execute("SELECT * from user")
 # data = cursor.fetchone()
-client = pymongo.MongoClient("mongodb://pig:123456@139.9.222.155:27017")
+client = pymongo.MongoClient(host="139.9.222.155", port=27017)
 db = client.trnodao
+db.authenticate("pig", "123456", mechanism='SCRAM-SHA-1')
+
 
 
--- code/views.py
+++ code/views.py
@@ -1,6 +1,6 @@
-import json,logging
+import json, logging
 import os
-import platform, random,time
+import platform, random, time
 from datetime import datetime
 
 import tornado.web
@@ -19,14 +19,13 @@ class MainHandler(tornado.web.RequestHandler):
         time_now = datetime.now()
         date = time_now.strftime("%Y-%m-%d")
         visit_num = data['visit_num']
-        ip_date = db.visit_list.find({"ip":remote_ip,"date":date})
+        ip_date = db.visit_list.find({"ip": remote_ip, "date": date})
         if ip_date.count() != 0:
             pass
         else:
-            db.visit_list.insert({"ip":remote_ip,"vist_time":time_now,"date":date})
-            db.visit.update_one({"visit_num":data['visit_num']},{"visit_num":data['visit_num']+1})
-            visit_num+=1
-
+            db.visit_list.insert({"ip": remote_ip, "vist_time": time_now, "date": date})
+            db.visit.update_one({"visit_num": data['visit_num']}, {"$set":{"visit_num": data['visit_num'] + 1}})
+            visit_num += 1
 
         self.render("index.html", **{"visit_num": visit_num, 'blog_num': data['blog_num']})
 
@@ -51,7 +50,7 @@ class UrlHandler(tornado.web.RequestHandler):
                 pass
             else:
                 db.visit_list.insert({"ip": remote_ip, "vist_time": time_now, "date": date})
-                db.visit.update_one({"visit_num": data['visit_num']}, {"visit_num": data['visit_num'] + 1})
+                db.visit.update_one({"visit_num": data['visit_num']}, {"$set":{"visit_num": data['visit_num'] + 1}})
                 visit_num += 1
 
             self.render("index.html", **{"visit_num": visit_num, 'blog_num': data['blog_num']})
@@ -71,18 +70,18 @@ class PageHandler(tornado.web.RequestHandler):
     def post(self):
         params = json.loads(self.request.body.decode('utf-8'))
         logging.info(params)
-        page = params.get('page',1)
-        per_page = params.get('per_page',10)
+        page = params.get('page', 1)
+        per_page = params.get('per_page', 10)
 
         data = db.text.find({})
         total_num = data.count()
         res_list = []
         if data:
-            for i in data[(int(page) - 1)*per_page:per_page]:
+            for i in data[(int(page) - 1) * per_page:per_page]:
                 adict = {}
                 adict['title'] = i["title"]
                 adict['text_id'] = i["text_id"]
-                adict['text'] = i["text"][:30]+'...' if len(i["text"]) > 30 else i["text"]
+                adict['text'] = i["text"][:30] + '...' if len(i["text"]) > 30 else i["text"]
                 adict['create_time'] = i["create_time"].strftime("%Y-%m-%d %H:%M")
                 res_list.append(adict)
 
@@ -97,8 +96,8 @@ class DetailHandler(tornado.web.RequestHandler):
         params = json.loads(self.request.body.decode('utf-8'))
         logging.info(params)
         text_id = params.get('text_id')
-        data = db.text.find({"text_id":text_id})
-
+        data = db.text.find({"text_id": text_id})
+        data = data[0]
         adict = {}
         adict['title'] = data["title"]
         adict['text_id'] = data["text_id"]
@@ -109,12 +108,11 @@ class DetailHandler(tornado.web.RequestHandler):
         adict['up'] = ""
         adict['next'] = ""
 
-
         total = db.text.find({}).count()
         if adict['text_id'] != '1':
-            adict['up'] = str(int(adict['text_id'])-1)
+            adict['up'] = str(int(adict['text_id']) - 1)
         if adict['text_id'] != str(total):
-            adict['next'] = str(int(adict['text_id'])+1)
+            adict['next'] = str(int(adict['text_id']) + 1)
         self.write(json_encode({"data": adict}))
 
 
@@ -135,7 +133,7 @@ class UploadImgHandler(tornado.web.RequestHandler):
             file_name = make_code_or_id("IMG") + img['filename'].split('.')[1]
             with open(new_dir + file_name, 'wb') as f:
                 f.write(img['body'])
-        self.write(json_encode({"uploaded":1,"url":"/wei/img/"+file_name}))
+        self.write(json_encode({"uploaded": 1, "url": "/wei/img/" + file_name}))
 
 
 class UploadTextHandler(tornado.web.RequestHandler):
@@ -149,5 +147,6 @@ class UploadTextHandler(tornado.web.RequestHandler):
         text = params.get('text')
         time_now = datetime.now()
         total_text = db.text.find({}).count()
-        db.text.insert({"text":text,"title":title,"text_id":str(total_text+1),"create_time":time_now,"create_user":"wei","type":"Python"})
-        self.write(json_encode({"text_id": str(total_text[0]+1)}))
+        db.text.insert({"text": text, "title": title, "text_id": str(total_text + 1), "create_time": time_now,
+                        "create_user": "wei", "type": "Python"})
+        self.write(json_encode({"text_id": str(total_text + 1)}))
